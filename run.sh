38b23a4764467517568130d749630b95f28470ca
zobrenovic <ml!X8!S!k&7f>
2022-02-04 13:23:05 +0100

    minor improvements

--- run.sh
+++ run.sh
@@ -4,11 +4,14 @@ node get-repos.js
 node generate-sh-scripts.js
 
 cd ../generated/clone-scripts
+rm clone-and-zip-*
 bash run-all.sh
 # or run-all-parallel.sh, to run cloning or repos for multiple organization in parallel
 
 cd ../analysis-scripts
+rm run-analysis-*
 bash run-all.sh
 
 cd ../pull-requests-scripts
+rm export-pull-requests-*
 bash run-all.sh
